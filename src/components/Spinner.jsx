f8584a71428792e4296570daf60e61d2886778f0
martinsifo <sifomartinm@gmail.com>
2023-03-07 21:12:55 -0300

    primer commit

---
+++ src/components/Spinner.jsx
@@ -0,0 +1,19 @@
+import "../styles/Spinner.css";
+
+const Spinner = () => {
+  return (
+    <div>
+      {" "}
+      <div className="sk-chase">
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+        <div className="sk-chase-dot"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Spinner;
