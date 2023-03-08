6cf4b3274d2dde58685aff0c26054d3da716def2
martinsifo <sifomartinm@gmail.com>
2023-03-07 23:22:15 -0300

    update spinner

--- src/components/Spinner.jsx
+++ src/components/Spinner.jsx
@@ -2,8 +2,7 @@ import "../styles/Spinner.css";
 
 const Spinner = () => {
   return (
-    <div>
-      {" "}
+    <div className="spinner-container">
       <div className="sk-chase">
         <div className="sk-chase-dot"></div>
         <div className="sk-chase-dot"></div>
